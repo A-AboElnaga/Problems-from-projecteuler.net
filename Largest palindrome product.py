501444f3c0e3a29a73207fa2342c30530dce3d8e
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2021-08-19 17:38:30 +0200

    Create Largest palindrome product.py

---
+++ Largest palindrome product.py
@@ -0,0 +1,25 @@
+import time
+def is_num_palindromic(num):
+    string = str(num)
+    string_rev = string[::-1]
+    if string_rev == string:
+        return True
+    else: 
+        return False
+start = time.time()
+
+n =3
+max_num = ((10**n) -1)
+min_num = (10**(n-1))
+start
+products = (z * x
+            for z in range(max_num, min_num - 1, -2)
+            for x in range(max_num, z - 1, -2)
+            if z * x % 11 == 0)
+for product in products:
+ if is_num_palindromic(product)==True:
+     end = time.time()
+     print(product,f'time:{end-start}')
+     break
+    
+        
