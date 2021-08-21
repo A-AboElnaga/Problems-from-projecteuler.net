56e3a0b1c00257d442211300ef3fd8b0cc8c52bc
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2021-08-21 15:35:10 +0200

    Update 3-Largest prime factor.py

--- 3-Largest prime factor.py
+++ 3-Largest prime factor.py
@@ -1,17 +1,16 @@
 import math
-def get_prime_list(num, primelist=[2,3,5,7,11]):
-    '''enter (num) an intger and returns list of all prime smaller than this number'''
-   
-    for i in range(primelist[-1]+1,num):
-        stat=[]
-        for prime in primelist:
-            if i % prime == 0:
-                stat.append(False)
-            else:
-                stat.append(True)
-        if False not in stat:
-                primelist.append(i)
-    return (primelist)
+def get_prime_list(num, primelist=[2, 3, 5, 7, 11]):
+       '''enter (num) an intger and returns list of all prime smaller than this number'''
+      
+       for i in range(primelist[-1],num,2):
+           is_num_prime = True
+           for prime in primelist :
+             if prime <= ((int(math.sqrt(num)))+1):
+               if i % prime == 0:
+                 is_num_prime = False
+           if is_num_prime == True :
+                   primelist.append(i)
+       return (primelist)
 num=600851475143 
 primelist= get_prime_list(10000) #I have limited my testing but this method is not that efficient a better one is Sieve of Eratosthenes algorithm 
 factors=[]
