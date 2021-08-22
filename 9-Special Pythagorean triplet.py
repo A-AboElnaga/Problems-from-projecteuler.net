e6268142fa7b9c08db354eaa9cbcde137c958dff
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2021-08-22 15:51:29 +0200

    Update 9-Special Pythagorean triplet.py

--- 9-Special Pythagorean triplet.py
+++ 9-Special Pythagorean triplet.py
@@ -1,22 +1,28 @@
+
 def fun(summ=1000):
-  
+  counter=0
   a,b,c=1,1,(summ-2)
-  while c>b and c>b :
-    while a < int(summ/2):
+  while c>a and c>b :
+    while a < int(summ/2) and  b> a :
        a_sqr =a**2
        b_sqr =b**2
        c_sqr =c**2
+       counter+=1
        if ((a_sqr+b_sqr)==c_sqr):
         print(f'{(a*a)} + {(b*b)} = {c*c} ')
         print(f'The Pythagorean triplet for which a + b + c = {summ} is[{a},{b},{c}]')
-        return a,b,c
+        return a,b,c,counter
        a +=1
        c -=1
     a=1
     b +=1
     c= summ-(a+b)
+  return counter
 try:
-  a,b,c =fun()
+  a,b,c,counter =fun()
   print(f'abc={a*b*c}')
-except: print('no such numbers exist')
-    
+  print(counter)
+except: 
+  print('no such numbers exist')
+  counter= fun()
+  print(counter)
