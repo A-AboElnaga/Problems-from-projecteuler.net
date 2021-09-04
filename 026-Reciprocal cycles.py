e66e42b88e61aa771b1d0a417fb2ae83dc512b91
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2021-09-04 14:53:25 +0200

    Update 026-Reciprocal cycles.py

--- 026-Reciprocal cycles.py
+++ 026-Reciprocal cycles.py
@@ -26,7 +26,7 @@ big=0
 d=0
 count=0
 dic={}
-for num in range(1,1001,1):
+for num in range(1,1001,1): #you can change the step to 2 and only search odd numbers because primes have the biggest factors and primes are all odd except 2
   count +=1
   length =Reciprocal_cycles(num)
   dic[num]=length
