e6b0747b3b5e74eb8699c5ebe4c9340b9b1f4212
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2021-09-04 14:50:15 +0200

    Update 026-Reciprocal cycles.py

--- 026-Reciprocal cycles.py
+++ 026-Reciprocal cycles.py
@@ -3,12 +3,9 @@ def Reciprocal_cycles(num):
   start= x
   counter=0
   while start!=0: 
-
-#change the counter limit to the max of your scanning limit
 #Primes p such that the decimal expansion of 1/p has period p-1
 #which is the greatest period possible for any integer.
-# I set it to 1000 beacause my search range is for numbers less than 1000
-      if counter >1000: 
+      if counter >num: 
         x =(x*10)%num
         start= x
         counter=0
@@ -22,7 +19,8 @@ def Reciprocal_cycles(num):
         return(counter)
   return(counter)
   
-
+import time
+start= time.time()
 
 big=0
 d=0
@@ -31,13 +29,14 @@ dic={}
 for num in range(1,1001,1):
   count +=1
   length =Reciprocal_cycles(num)
-  dic[num]=length #storing the data because its cool
+  dic[num]=length
   if length > big:
     big = length
     d = num
-  if count >1002: #You may delete this if condition but i kept it because I usually get stuck in infinite loops, so I use breaks
+  if count >1002:
     break
 print(d,big)
-
+end = time.time()
+print(end-start)
 
     
