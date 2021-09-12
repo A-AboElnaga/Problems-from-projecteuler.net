404da4f24cde02af1d46976ab59567937a9fb47e
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2021-09-13 01:27:04 +0200

    Update 031-Coin sums.py

--- 031-Coin sums.py
+++ 031-Coin sums.py
@@ -1,3 +1,4 @@
+#Method1: Brute Force
 coins=[1,2,5,10, 20, 50, 100,200]
 total=200
 ways=0
@@ -10,3 +11,20 @@ for a in range(total,-1,-200):
             for g in range(f,-1,-2):
               ways+=1
 print(ways) 
+  
+      
+#Method2: Dynamic programing Recursive Depth First Search with Memoization 
+
+def fun(total, coins,const,memo={}):
+  if total==0:return 1
+  ans=0
+  for coin in coins:
+    rem= total - coin
+    if str(total)+'-'+str(const) in memo:
+      return memo[str(total)+'-'+str(const)]
+    if rem>=0 and coin>=const:
+      ans+= fun(rem,coins,coin)
+  memo[str(total)+'-'+str(const)]=ans
+  return ans
+target=200
+print(fun(target,coins,0))
