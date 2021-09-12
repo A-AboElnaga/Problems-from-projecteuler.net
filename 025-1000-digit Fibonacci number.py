e50f2f05caff353f26e933a908cbc7e6416e32c9
A. Aboelnaga <77728721+A-AboElnaga@users.noreply.github.com>
2021-09-12 12:56:34 +0200

    Update 025-1000-digit Fibonacci number.py

--- 025-1000-digit Fibonacci number.py
+++ 025-1000-digit Fibonacci number.py
@@ -1,3 +1,5 @@
+import time
+start = time.time()
 def fib(n, memo={}):
     if n == 0 or n == 1:
         return 1
@@ -9,9 +11,26 @@ def fib(n, memo={}):
         return result
 
 if __name__ == '__main__':
-  x,y='',2000
+  x,y='',1
   while len(x) <1000:
     y+=1
     x=str(fib(y))
 print(y+1)
-    
+print(f'time:{time.time()-start}')
+
+
+
+start = time.time()
+def fib1(num,fib_list=[1,1]):
+    fib_list.append(fib_list[-1]+fib_list[-2])
+    fib_list.pop(0)
+    return(fib_list[-1],fib_list)
+
+fib_list=[1,1]
+x,y='',1
+while len(x) <1000:
+  y+=1
+  x,fib_list=(fib1(y,fib_list))
+  x=str(x)
+print(y+1)
+print(f'time:{time.time()-start}')
